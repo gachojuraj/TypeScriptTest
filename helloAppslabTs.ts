c3c26fe3c7d96bedadc0811244ea55e268556a35
gachojuraj <mmamma151@gmail.com>
2020-10-05 19:57:15 +0200

    Initial commit

---
+++ helloAppslabTs.ts
@@ -0,0 +1,4 @@
+console.log("Hello AppsLab 2020!");
+
+var str:string = "This is string";
+console.log(str);
---
+++ out/helloAppslabTs.js
@@ -0,0 +1,4 @@
+console.log("Hello AppsLab 2020!");
+var str = "This is string";
+console.log(str);
+//# sourceMappingURL=helloAppslabTs.js.map
